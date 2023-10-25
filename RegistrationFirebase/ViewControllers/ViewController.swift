f7c0078cce99db1d45c6fc2e8196d6bf0d7c96a5
Daria <dasharahman009@gmail.com>
2023-10-25 22:08:23 +0300

    Adding a video splash screen to the home screen using AVKit

--- RegistrationFirebase/ViewControllers/ViewController.swift
+++ RegistrationFirebase/ViewControllers/ViewController.swift
@@ -6,17 +6,27 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    
+    var videoPlayer:AVPlayer?
+    
+    var videoPlayerLayer:AVPlayerLayer?
+    
     @IBOutlet weak var signUpButton: UIButton!
     
     @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       setUpElements()
+        
+        setUpElements()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Set up video in the background
+        setUpVideo()
     }
     
     func setUpElements(){
@@ -25,5 +35,34 @@ class ViewController: UIViewController {
         Utilities.styleHollowButton(loginButton)
     }
     
-
+    func setUpVideo(){
+        
+        // Get the path to the resource in the bundle
+        let bundlePath = Bundle.main.path(forResource: "video", ofType: "mov")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        // Create a URL from it
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Create the video player item
+        let item = AVPlayerItem(url: url)
+        
+        // Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        // Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        // Adjust the size and frame
+        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        // Add it to the view and play it
+        videoPlayer?.playImmediately(atRate: 0.3)
+    }
+    
 }
